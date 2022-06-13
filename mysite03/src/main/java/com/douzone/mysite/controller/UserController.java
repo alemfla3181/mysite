b6f6a2e59ebc4ff876400b583dbbfc100697cc6f
Dragon <alemfla3181@naver.com>
2022-06-13 12:20:02 +0900

    AuthInterceptrt + @annotation

--- mysite03/src/main/java/com/douzone/mysite/controller/UserController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.douzone.mysite.security.Auth;
 import com.douzone.mysite.service.UserService;
 import com.douzone.mysite.vo.UserVo;
 
@@ -39,27 +40,15 @@ public class UserController {
 		return "user/login";
 	}
 
-	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public String login(HttpSession session, UserVo vo, Model model) {
-		UserVo authUser = userService.getUser(vo);
-		if (authUser == null) {
-			model.addAttribute("result", "fail");
-			model.addAttribute("email", vo.getEmail());
-			return "user/login";
-		}
-
-		// 인증 처리
-		session.setAttribute("authUser", authUser);
-		return "redirect:/";
+	@RequestMapping(value = "/auth")
+	public void auth() {
 	}
 
 	@RequestMapping("/logout")
-	public String logout(HttpSession session) {
-		session.removeAttribute("authUser");
-		session.invalidate();
-		return "redirect:/";
+	public void logout() {
 	}
 
+	@Auth()
 	@RequestMapping(value = "/update", method = RequestMethod.GET)
 	public String update(HttpSession session, Model model) {
 
@@ -85,11 +74,11 @@ public class UserController {
 			return "redirect:/";
 		}
 		// ----------------------------------------------
-		
+
 		vo.setNo(authUser.getNo());
 		userService.updateUser(vo);
 		authUser.setName(vo.getName());
-		
+
 		return "redirect:/user/update";
 	}
 }
